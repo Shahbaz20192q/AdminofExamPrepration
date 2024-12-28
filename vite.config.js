4d6d5c44aad1c82a8c53e22ec6969ee95e8fbf61
Shahbaz20192q <shahbazghaffar00@gmail.com>
2024-12-28 13:21:49 +0500

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/AdminofExamPrepration/", // Set the base path for the project
   plugins: [react()],
-})
+});
