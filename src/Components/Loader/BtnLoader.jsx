a5ed471768a01fd786d8f5054c41731993737fce
Shahbaz20192q <shahbazghaffar00@gmail.com>
2025-01-05 21:48:53 +0500

    remove unused CSS and component files to clean up the project

--- src/Components/Loader/BtnLoader.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const BtnLoader = () => {
-  return <div className="btn-loader loader"></div>;
-};
-
-export default BtnLoader;
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/AdminofExamPrepration/", // Set the base path for the project
-  plugins: [react()],
-});
